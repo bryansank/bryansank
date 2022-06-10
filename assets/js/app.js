f776b52c792f7e590d7b1d840165d6edb347a036
Bryan <bryansan055@gmail.com>
2022-06-10 15:19:06 -0400

    todo: need responsive all page, i need add carrousel.

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,26 @@ punto.forEach( (point, i)=>{
     });
 })
 //
+
+function sendEmail(){
+    let contact_me_name = document.getElementById('contact_me_name').value;
+    let contact_me_email = document.getElementById('contact_me_email').value;
+    let contact_me_message = document.getElementById('contact_me_message').value;
+
+    if(contact_me_name, contact_me_email, contact_me_message){
+        // const objContact = {name: contact_me_name, email: contact_me_email, message: contact_me_message};
+        const formatMessageEmail = `${contact_me_name} te contacta mediante: ${contact_me_email} \n\n Y dice lo siguiente: ${contact_me_message}.`;
+
+        sendEmailForMyAccountSECONDARY(formatMessageEmail, `Contacto de ${contact_me_name}`, contact_me_email, "");   
+    }else{
+        // TODO: Cambiar mensaje temporal
+        alert('Por favor complete los campos.')
+    }
+    
+}
+
+function sendEmailForMyAccountSECONDARY(message="Without Text :c", subject="Hi! I need comunication with you.", from="", copyTo = ""){
+    const myEmail_spam = "bryansan055";
+    // Coloco el email separado... Para evitar los bots.
+    window.open(`mailto:${myEmail_spam}@gmail.com?cc=${copyTo}&subject=${subject}&body=${message}`);
+}
